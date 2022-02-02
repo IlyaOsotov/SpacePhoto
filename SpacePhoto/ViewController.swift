ab8983b2ee47439c5f8f85a59c083955af472ecc
IlyaOsotov <soten58@yandex.ru>
2022-02-02 13:05:24 +0700

    init

--- SpacePhoto/ViewController.swift
+++ SpacePhoto/ViewController.swift
@@ -6,14 +6,73 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var photoView: UIImageView!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var copyrightLabel: UILabel!
+    @IBOutlet var webView: WKWebView!
+    
+    let photoInfoController = PhotoInfoController()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = ""
+        photoView.image = UIImage(systemName: "photo.on.rectangle")
+        descriptionLabel.text = ""
+        copyrightLabel.text = ""
+                
+        photoInfoController.fetchPhotoInfo { (result) in
+            switch result {
+            case .success(let photoInfo):
+                self.updateUI(with: photoInfo)
+            case .failure(let error):
+                self.updateUI(with: error)
+            }
+        }
     }
+    
+    func updateUI(with photoInfo: PhotoInfo) {
+        photoInfoController.fetchImage(from: photoInfo.url) { (result) in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let image):
+                    self.webView.isHidden = true
+                    self.title = photoInfo.title
 
+                    self.photoView.image = image
+                    self.descriptionLabel.text = photoInfo.description
+                    self.copyrightLabel.text = photoInfo.copyright
+                case .failure(let error):
+                    if photoInfo.url.absoluteString.contains("youtube") {
+                        self.updateUI(with: photoInfo, videoURL: photoInfo.url)
+                    } else {
+                        self.updateUI(with: error)
+                    }
+                }
+            }
+        }
+    }
+    
+    func updateUI(with photoInfo: PhotoInfo, videoURL: URL) {
+        self.photoView.isHidden = true
+        self.title = photoInfo.title
+        self.descriptionLabel.text = photoInfo.description
+        self.copyrightLabel.text = photoInfo.copyright
+        
+        let myRequest = URLRequest(url: videoURL)
+        self.webView.load(myRequest)
+    }
+    
+    func updateUI(with error: Error) {
+        title = "Error fetching photo"
+        photoView.image = UIImage(systemName: "exclamationmark.octagon")
+        descriptionLabel.text = error.localizedDescription
+        copyrightLabel.text = ""
+    }
 
 }
 
